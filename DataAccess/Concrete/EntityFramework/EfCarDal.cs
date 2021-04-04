6e197aaeed771e4551ff93b1d70138b79249ac4b
Davutb5454 <67680075+Davutb5454@users.noreply.github.com>
2021-04-04 16:28:21 +0300

    DataAccess refactor

---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : EfEntityRepositoryBase<Car, ReCapProjectContext>, ICarDal
+    {
+        public List<CarDetailDto> GetCarDetails(Expression<Func<CarDetailDto, bool>> filter = null)
+        {
+            using (var context = new ReCapProjectContext())
+            {
+                var result = from p in context.Cars
+                    join b in context.Brands on p.BrandId equals b.Id
+                    join c in context.Colors on p.ColorId equals c.Id
+                    select new CarDetailDto
+                    {
+                        Id = p.Id,
+                        CarName = p.Name,
+                        BrandName = b.Name,
+                        ColorName = c.Name,
+                        DailyPrice = p.DailyPrice,
+                        ModelYear = p.ModelYear
+                    };
+
+                return filter == null ? result.ToList() : result.Where(filter).ToList();
+            }
+        }
+    }
+}
+
