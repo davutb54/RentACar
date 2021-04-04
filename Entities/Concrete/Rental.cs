e9962bf0403df3fdd715f892ed052c6b6662238d
Davutb5454 <67680075+Davutb5454@users.noreply.github.com>
2021-04-04 16:25:08 +0300

    Entity

---
+++ Entities/Concrete/Rental.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Core.Entities;
+
+namespace Entities.Concrete
+{
+    public class Rental : IEntity
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime RentStartDate { get; set; }
+        public DateTime RentEndDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+    }
+}
---
+++ Entities/DTOs/CarDetailDto.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+
+namespace Entities.DTOs
+{
+    public class CarDetailDto : IDto
+    {
+        public int Id { get; set; }
+        public string CarName { get; set; }
+        public string BrandName { get; set; }
+        public string ColorName { get; set; }
+        public decimal DailyPrice { get; set; }
+        public short ModelYear { get; set; }
+    }
+}
